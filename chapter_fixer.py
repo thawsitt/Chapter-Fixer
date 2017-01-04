06ea95caa378187a2fe354090ab22391d36ac31f
thawsitt <thawsitt@cs.stanford.edu>
2017-01-03 21:47:41 -0500

    Run the script throught Pylint. Increased score from 03/10 to 10/10

--- chapter_fixer.py
+++ chapter_fixer.py
@@ -1,12 +1,12 @@
 # Chapter_fixer (v1.1)
 # --------------------
-# Author: Thawsitt Naing 
+# Author: Thawsitt Naing
 # Date: Dec 2016
 # Description: A simple Python script that does search and replace. Replaces all occurences of
 # the string 'Unable' in the chapter file with the correct timing (eg: '00:01:37.000').
 
 '''
-Pre-condition: The script is placed in the same folder as the chapter file to edit. 
+Pre-condition: The script is placed in the same folder as the chapter file to edit.
 The chapter file should be in the following format: <name><ep. num>_Chapter.xml
 You have to type in the episode number. For example, we are going to edit a chapter file
 called 'aot1_Chapter.xml'
@@ -15,34 +15,40 @@ Post-condition: All occurences of the string 'Unable' in the chapter file will b
 with the correct timing supplied in the script. (eg: 00:01:30). The original (unfixed) chapter
 file will be saved with the suffix '_backup.xml'
 '''
-
 import os
 
-# You need to edit the following 3 values if you are using the script for the first time. 
 
-title = 'Yuri on ice!!'     # edit anime title
-episode_name = 'yuri'       # edit name of the video file.
-timing = '00:01:37.000'     # edit Correct Time
-  
-print('-----------------------------------------------')
-print('Fix chapter file for: ' + title)
-print('-----------------------------------------------')
+def main():
+    """ Fixes the chapter file by replacing all instances of 'unable' with the correct timing. """
 
-episode_num = input("Enter episode number: ")
-file_to_open = episode_name + str(episode_num) + '_Chapter.xml'
-file_to_write = episode_name + str(episode_num) + '_Chapter_Edited.xml'
+    # You need to edit the following 3 values if you are using the script for the first time.
+    title = 'Yuri on ice!!'     # edit anime title
+    episode_name = 'yuri'       # edit name of the video file.
+    timing = '00:01:37.000'     # edit Correct Time
 
-f_in = open(file_to_open, 'r')
-f_out = open(file_to_write, 'w')
+    print('-----------------------------------------------')
+    print('Fix chapter file for: ' + title)
+    print('-----------------------------------------------')
 
-for line in f_in:
-    f_out.write(line.replace('Unable', timing))
+    episode_num = input("Enter episode number: ")
+    file_to_open = episode_name + str(episode_num) + '_Chapter.xml'
+    file_to_write = episode_name + str(episode_num) + '_Chapter_Edited.xml'
 
-f_in.close()
-f_out.close()
+    f_in = open(file_to_open, 'r')
+    f_out = open(file_to_write, 'w')
 
-backup_file = episode_name + str(episode_num) + '_Chapter_backup.xml'
-os.rename(file_to_open, backup_file)
-os.rename(file_to_write, file_to_open)
+    for line in f_in:
+        f_out.write(line.replace('Unable', timing))
 
-print('Done!')
+    f_in.close()
+    f_out.close()
+
+    backup_file = episode_name + str(episode_num) + '_Chapter_backup.xml'
+    os.rename(file_to_open, backup_file)
+    os.rename(file_to_write, file_to_open)
+
+    print('Done!')
+
+
+if __name__ == '__main__':
+    main()
